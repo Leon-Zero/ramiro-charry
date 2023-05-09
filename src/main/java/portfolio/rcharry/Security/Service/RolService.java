9ae6e6d298d0d93295e88ef9a924df78344e53a2
Ramiro Charry <ramiro22_cba@hotmail.com>
2023-05-09 20:30:07 -0300

    commit
    subida a para el deploy

---
+++ src/main/java/portfolio/rcharry/Security/Service/RolService.java
@@ -0,0 +1,23 @@
+
+package portfolio.rcharry.Security.Service;
+
+import portfolio.rcharry.Security.Entity.Rol;
+import portfolio.rcharry.Security.Enums.RolNombre;
+import portfolio.rcharry.Security.Repository.RolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    
+    @Autowired
+    RolRepository rolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return rolRepository.findByRolNombre(rolNombre);
+    }     
+    
+}
